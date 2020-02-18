5a7722a5ebc0ee8d5edae4f511081ea719c4e4dd
TheBudderBomb <frywilliam6@gmail.com>
2020-02-18 16:42:44 -0600

    Adding in a pano camera script, still showing a couple errors with position rules.

---
+++ FrySpice IP/Assets/Scripts/Panoramic_Camera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panoramic_Camera : MonoBehaviour
+{
+    //init of player
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player)
+        {
+            if (player.transform.position.x > -1)
+            {
+                if(player.transform.position.y > -1)
+                {
+                    transform.position = transform.position.y;
+                }
+
+                transform.position = transform.position.x;
+
+            }
+        }
+    }
+}
