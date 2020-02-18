a12f5d68358d05ade3407a6e4e5d4a4bf28d6988
TheBudderBomb <frywilliam6@gmail.com>
2020-02-18 13:45:07 -0600

    Added some destroyer functionality, if player 'dies', the scene pauses and reverts to editor.

---
+++ FrySpice IP/Assets/Scripts/Fall_Kill.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fall_Kill : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //Trigger method, tells the game when something crosses the line of the destroyer.
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //If the trigger happens to be tagged as a 'Player', does this.
+        if (other.tag == "Player")
+        {
+            Debug.Break();
+            return;
+        }
+
+        if (other.gameObject.transform.parent)
+        {
+            Destroy(other.gameObject.transform.parent.gameObject);
+        } else {
+            Destroy(other.gameObject);
+        }
+    }
+}
