cc279176cd5c9afa49f01b06b25d6c139367c101
Josh Schmitz <joshschmitz16@outlook.com>
2020-02-19 21:18:03 -0600

    portal added

---
+++ FrySpice IP/Assets/Scripts/Portal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    /*
+    Author: Joshua Schmitz
+    Date: Feb 19, 2020
+    Description: Portal script for when player touches portal allows player to 
+    teleport to new location determined by portal
+    */
+    public GameObject myPortal;
+    public GameObject Player;
+    
+
+    public void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.tag == "Player")
+        {
+            StartCoroutine (Teleport());
+        }
+    }
+
+    IEnumerator Teleport(){
+        yield return new WaitForSeconds (0);
+        Player.transform.position = new Vector2(myPortal.transform.position.x,myPortal.transform.position.y);
+    }
+}
